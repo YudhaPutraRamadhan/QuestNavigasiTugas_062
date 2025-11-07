5c70a4a5f1ea1a4b6087e9950cd3f844bc697e8a
MzSuwawa <yudharamadhan838@gmail.com>
2025-11-07 09:50:59 +0900

    Membuat file composable untuk Halaman Utama

---
+++ app/src/main/java/com/example/navigasilayout/view/HomeScreen.kt
@@ -0,0 +1,10 @@
+package com.example.navigasilayout.view
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavController
+import androidx.navigation.NavHostController
+
+@Composable
+fun HomeScreen(navController: NavHostController) {
+
+}
