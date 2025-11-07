795bdcde7a2dacfcf0dfeb93b39afd683652c25c
MzSuwawa <yudharamadhan838@gmail.com>
2025-11-07 20:30:16 +0900

    Menambahkan NavHost dan route untuk setiap layar

--- app/src/main/java/com/example/navigasilayout/Navigasi.kt
+++ app/src/main/java/com/example/navigasilayout/Navigasi.kt
@@ -1,11 +1,17 @@
 package com.example.navigasilayout
 
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.navigation.NavController
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import com.example.navigasilayout.view.Formulir
+import com.example.navigasilayout.view.HomeScreen
+import com.example.navigasilayout.view.TampilData
 
 enum class Navigasi {
     HomeScreen,
@@ -14,15 +20,43 @@ enum class Navigasi {
 }
 
 @Composable
-fun NavigasiApp(navController: NavHostController = rememberNavController())
-{
-    NavHost(navController = navController, startDestination = Navigasi.HomeScreen.name)
-    {
-        composable(route = Navigasi.HomeScreen.name) {
-        }
-        composable(route = Navigasi.DataPeserta.name) {
-        }
-        composable(route = Navigasi.Formulir.name) {
+fun DataApp(
+    navController: NavHostController = rememberNavController(),
+    modifier: Modifier
+){
+    Scaffold { isiRuang->
+        NavHost(navController = navController,
+                startDestination = Navigasi.HomeScreen.name,
+                modifier = Modifier.padding(isiRuang))
+        {
+            composable(route = Navigasi.HomeScreen.name) {
+                HomeScreen(
+                    OnToFormBtnClick = {
+                        navController.navigate(Navigasi.Formulir.name)
+                    }
+                )}
+            composable(route = Navigasi.DataPeserta.name) {
+                TampilData(
+                    OnToFormBtnClick = {
+                        navController.popBackStack()
+                    },
+                    OnToHomeBtnClick = {
+                        navController.navigate(Navigasi.HomeScreen.name) {
+                            popUpTo(Navigasi.HomeScreen.name) {
+                                inclusive = true
+                            }
+                            launchSingleTop = true
+                        }
+                    }
+                )
+            }
+            composable(route = Navigasi.Formulir.name) {
+                Formulir(
+                    OnSubmitButtonClick = {
+                        navController.navigate(Navigasi.DataPeserta.name)
+                    }
+                )
+            }
         }
     }
 }
