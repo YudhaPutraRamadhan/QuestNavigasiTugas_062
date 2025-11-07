0dc7ea9189c8df3b5710720840570c4164ad120c
MzSuwawa <yudharamadhan838@gmail.com>
2025-11-07 09:48:58 +0900

    Menambahkan NavHost dan composable route

--- app/src/main/java/com/example/navigasilayout/Navigasi.kt
+++ app/src/main/java/com/example/navigasilayout/Navigasi.kt
@@ -1,7 +1,28 @@
 package com.example.navigasilayout
 
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavController
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+
 enum class Navigasi {
     HomeScreen,
     DataPeserta,
     Formulir
+}
+
+@Composable
+fun NavigasiApp(navController: NavHostController = rememberNavController())
+{
+    NavHost(navController = navController, startDestination = Navigasi.HomeScreen.name)
+    {
+        composable(route = Navigasi.HomeScreen.name) {
+        }
+        composable(route = Navigasi.DataPeserta.name) {
+        }
+        composable(route = Navigasi.Formulir.name) {
+        }
+    }
 }
