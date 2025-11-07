18c2174c8e71fd3c91ca3dd0181a969223ee5a9c
MzSuwawa <yudharamadhan838@gmail.com>
2025-11-07 09:53:45 +0900

    Menambahkan Surface pada HomeScreen

--- app/src/main/java/com/example/navigasilayout/view/HomeScreen.kt
+++ app/src/main/java/com/example/navigasilayout/view/HomeScreen.kt
@@ -1,10 +1,16 @@
 package com.example.navigasilayout.view
 
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.core.content.pm.ShortcutInfoCompat
 import androidx.navigation.NavController
 import androidx.navigation.NavHostController
 
 @Composable
 fun HomeScreen(navController: NavHostController) {
+    Surface (modifier = Modifier.fillMaxSize()) {
 
+    }
 }
