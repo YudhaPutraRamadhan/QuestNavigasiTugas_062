8ee9482aa0fe92d2bb9c836bf4b6628ac61e197f
MzSuwawa <yudharamadhan838@gmail.com>
2025-11-07 09:57:25 +0900

    Menambahkan Spacer pada HomeScreen

--- app/src/main/java/com/example/navigasilayout/view/HomeScreen.kt
+++ app/src/main/java/com/example/navigasilayout/view/HomeScreen.kt
@@ -2,13 +2,16 @@ package com.example.navigasilayout.view
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.core.content.pm.ShortcutInfoCompat
 import androidx.navigation.NavController
@@ -23,6 +26,8 @@ fun HomeScreen(navController: NavHostController) {
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
             Text("Selamat Datang", fontSize = 30.sp, fontWeight = FontWeight.Bold)
+
+            Spacer(modifier = Modifier.height(24.dp))
         }
     }
 }
