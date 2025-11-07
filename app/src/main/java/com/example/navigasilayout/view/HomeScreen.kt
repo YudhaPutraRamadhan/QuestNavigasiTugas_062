7f8a17eb0dfbf3dbe19592215ec60dd64dabe4f1
MzSuwawa <yudharamadhan838@gmail.com>
2025-11-07 09:56:23 +0900

    Menambahkan teks "Selamat Datang" di halaman utama

--- app/src/main/java/com/example/navigasilayout/view/HomeScreen.kt
+++ app/src/main/java/com/example/navigasilayout/view/HomeScreen.kt
@@ -4,9 +4,12 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
 import androidx.core.content.pm.ShortcutInfoCompat
 import androidx.navigation.NavController
 import androidx.navigation.NavHostController
@@ -19,7 +22,7 @@ fun HomeScreen(navController: NavHostController) {
             verticalArrangement = Arrangement.Center,
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
-
+            Text("Selamat Datang", fontSize = 30.sp, fontWeight = FontWeight.Bold)
         }
     }
 }
