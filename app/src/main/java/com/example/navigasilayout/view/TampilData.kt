9b0eac81b2a2d9297cdf563e6f90fdeeaa6d506d
MzSuwawa <yudharamadhan838@gmail.com>
2025-11-07 20:25:28 +0900

    Mendesain ulang layout TampilData dan menambahkan fungsi navigasi

--- app/src/main/java/com/example/navigasilayout/view/TampilData.kt
+++ app/src/main/java/com/example/navigasilayout/view/TampilData.kt
@@ -1,6 +1,7 @@
 package com.example.navigasilayout.view
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
@@ -12,14 +13,23 @@ import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.stringArrayResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -27,58 +37,56 @@ import androidx.lifecycle.ViewModel
 import androidx.navigation.NavController
 import com.example.navigasilayout.R
 
+@OptIn(ExperimentalMaterial3Api::class)
+
 @Composable
-fun TampilData(navController: NavController, viewModel: Any) {
-    val pesertaList = stringArrayResource(id = R.array.data_peserta)
-    val gradient = Brush.verticalGradient(
-        colors = listOf(Color.Gray, Color.Blue)
+fun TampilData(
+    modifier: Modifier = Modifier,
+    OnToFormBtnClick: () -> Unit,
+    OnToHomeBtnClick: () -> Unit,
+) {
+    val items = listOf(
+        Pair(stringResource(R.string.nama_lengkap), "Yudha Ramadhan"),
+        Pair(stringResource(R.string.jenis_kelamin), "Laki-Laki"),
+        Pair(stringResource(R.string.alamat), "Yogyakarta")
     )
-    Surface (modifier = Modifier.fillMaxSize()) {
-        Column(
-            modifier = Modifier
-                .background(gradient)
-                .fillMaxSize()
-                .padding(16.dp),
-            horizontalAlignment = Alignment.CenterHorizontally
-        ) {
-            Text(
-                text = "List Daftar Peserta",
-                fontSize = 24.sp,
-                fontWeight = FontWeight.Bold,
-                color = Color.White
+    Scaffold(modifier = Modifier,
+        {
+            TopAppBar(
+                title = {Text(text = stringResource(id = R.string.tampil),
+                    color = Color.White
+                )},
+                colors = TopAppBarDefaults
+                    .mediumTopAppBarColors(colorResource
+                        (id = R.color.teal_700)
+                    )
             )
-
-            Spacer(modifier = Modifier.height(20.dp))
-
-            pesertaList.forEach { peserta ->
-                Card(
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .padding(vertical = 6.dp),
-                    shape = RoundedCornerShape(16.dp),
-                    elevation = CardDefaults.cardElevation(8.dp)
-                ) {
-                    Column(modifier = Modifier.padding(16.dp)) {
-                        Text(text = peserta, fontSize = 16.sp)
+        }){ isiRuang->
+        Column(modifier = Modifier.padding(isiRuang),
+            verticalArrangement = Arrangement.SpaceBetween) {
+            Column(modifier = Modifier.padding(dimensionResource(id = R.dimen.padding_medium)),
+                verticalArrangement = Arrangement.spacedBy(dimensionResource(id = R.dimen.padding_small))) {
+                items.forEach { item ->
+                    Column {
+                        Text(text = item.first.uppercase(),
+                            fontSize = 16.sp)
+                        Text(text = item.second,
+                            fontWeight = FontWeight.Bold,
+                            fontFamily = FontFamily.SansSerif,
+                            fontSize = 22.sp)
                     }
+                    HorizontalDivider(thickness = 1.dp, color = Color.Cyan)
                 }
-
-                Spacer(modifier = Modifier.height(20.dp))
-
+                Spacer(modifier = Modifier.height(height = 10.dp))
                 Button(
-                    onClick = { navController.navigate("HomeScreen") },
-                    modifier = Modifier.fillMaxWidth().padding(horizontal = 32.dp)
-                ) {
-                    Text("Beranda")
+                    modifier = Modifier.fillMaxWidth(),
+                    onClick = OnToFormBtnClick) {
+                    Text(text = stringResource(id=R.string.FormulirPendaftaran))
                 }
-                Spacer(modifier = Modifier.height(12.dp))
-
                 Button(
-                    onClick = { navController.navigate("Formulir") },
-                    colors = ButtonDefaults.buttonColors(containerColor = Color.White),
-                    modifier = Modifier.fillMaxWidth().padding(horizontal = 32.dp)
-                ) {
-                    Text("Formulir Pendaftarn", color = Color.White)
+                    modifier = Modifier.fillMaxWidth(),
+                    onClick = OnToHomeBtnClick) {
+                    Text(text = stringResource(id=R.string.home))
                 }
             }
         }
