2ba3b6d876730f4c9d9f83f7e77c0c92336f33e2
MzSuwawa <yudharamadhan838@gmail.com>
2025-11-07 09:54:58 +0900

    Membuat layout Column di HomeScreen

--- app/src/main/java/com/example/navigasilayout/view/HomeScreen.kt
+++ app/src/main/java/com/example/navigasilayout/view/HomeScreen.kt
@@ -1,8 +1,11 @@
 package com.example.navigasilayout.view
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.core.content.pm.ShortcutInfoCompat
 import androidx.navigation.NavController
@@ -11,6 +14,12 @@ import androidx.navigation.NavHostController
 @Composable
 fun HomeScreen(navController: NavHostController) {
     Surface (modifier = Modifier.fillMaxSize()) {
+        Column (
+            modifier = Modifier.fillMaxSize(),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
 
+        }
     }
 }
